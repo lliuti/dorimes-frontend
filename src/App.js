71d646cf996c26bdb1026ccca473ed57e02bd25b
lliuti <dev.lliuti@gmail.com>
2020-02-20 12:43:27 -0300

    Importing location component to App

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import React from 'react';
 import './App.css';
 import Home from './components/Home/index';
 import Menu from './components/Menu/index';
+import Location from './components/Location/index';
 
 function App() {
   return (
     <>
       <Home/>
       <Menu/>
+      <Location/>
     </>
   );
 }
